26b4c9493285f5022420eaad096adeb646e4b376
League <league@WTS8.attlocal.net>
2019-04-01 18:26:01 -0700

    Happy pet, pi aloud, string methods (ip)
    :neutral_face:

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -1,5 +1,7 @@
 package charAt_scanners_optionDialogs;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
@@ -8,29 +10,41 @@ public class HappyPet {
 	// 0;
 	// this will be used to store the happiness of your pet
 	static int happinessLevel = 0;
+	static Random rand = new Random();
+	static int a = rand.nextInt(2);
 
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in
 		// a variable
+
 		String x = JOptionPane.showInputDialog("What kind of pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		for (int y = 0; y < 6; y++) {
+			a = rand.nextInt(2);
 			// 3. Use showOptionDialog to ask the user what they want to do to make their
 			// pet happy
 			// (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			// Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "What Do you want to do to make your pet happy?", "Title", 0,
+			int task = JOptionPane.showOptionDialog(null, "What Do you want to do to make your pet happy?", "Activities", 0,
 					JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Cuddle", "Take on walk", "Feed" }, null);
-
+System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			JOptionPane.showMessageDialog(null, nextString(task));
 			// 6. If you determine the happiness level is large enough, tell the
 			// user that he loves his pet and use break; to exit for loop.
 		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
-	// Each method should create a pop-up with the pet's response (eg. cat might
-	// purr when pet),
-	// and INCREMENT the pet's happiness Level.
+	public static String nextString(int task) {
+		// Each method should create a pop-up with the pet's response (eg. cat might
+		// purr when pet),
+		// and INCREMENT the pet's happiness Level.
+		if (task == 0) {
+			return "Your pet is happy";
+		} else if (task == 1) { 
+			return "Your pet is having fun";
+		} else 
+			return "Your pet is well fed";
+	}
 }
--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -8,16 +8,15 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String x = "Mynameisjeff";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
-		// 3. Print the length of your String to the console.
+System.out.println(x.charAt(3));		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(x.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+for (int y = 0; y<x.length(); y++){System.out.println(x.charAt(y));}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
