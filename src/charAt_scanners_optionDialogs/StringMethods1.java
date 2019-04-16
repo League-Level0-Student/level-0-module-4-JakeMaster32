74a8b387285e06574e9c5b74303539cdae867584
JakeMaster32 <league@iMac-2.attlocal.net>
2019-04-15 18:28:56 -0700

    String methods, change calculator, sleepy head (ip)
    :confused:

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -1,30 +1,40 @@
 package charAt_scanners_optionDialogs;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
+import javax.xml.bind.ParseConversionEvent;
+
 //    Level 0
-
-
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
+
 		// 1. Create a String variable and initialize it to whatever you want
-String x = "Mynameisjeff";
+		String x = "Mynameisjeff";
 		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-System.out.println(x.charAt(3));		// 3. Print the length of your String to the console.
-		//    HINT: .length()
-System.out.println(x.length());
+		// HINT: .charAt
+		System.out.println(x.charAt(3)); // 3. Print the length of your String to the console.
+		// HINT: .length()
+		System.out.println(x.length());
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
-for (int y = 0; y<x.length(); y++){System.out.println(x.charAt(y));}
-		
+		// HINT: use .length() to determine how many loops
+		for (int y = 0; y < x.length(); y++) {
+			System.out.println(x.charAt(y));
+		}
+
 		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+		// what position/index in the String the char is located.
+		// Print the char's position to the console.
+		// EXAMPLE: if your string is "abc" and you are searching
+		// for char 'b',  then print "b is at index 1"
+		char a = 'j';
+		String index = "char not found";
+		for (int z = 0; z < x.length(); z++) {
+			if (x.charAt(z) == a) {
+				System.out.println(""+a+ " is at index "+z);
+				 index = "char found at; "+z;
+			}
+			
+		}
+		System.out.println(index);
 	}
 }
-
-
--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -1,7 +1,7 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
@@ -15,17 +15,22 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickel = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		double nickel2 = Integer.parseInt(nickel);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dime = JOptionPane.showInputDialog("How many dimes do you have?");
+		double dime2 = Integer.parseInt(dime);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+		String quarter = JOptionPane.showInputDialog("How many quarters do you have");
+		double quarter2 = Integer.parseInt(quarter);
+		// Calculate how much money the user has and save it in a double variable
+		double money = 0;
+		nickel2 = nickel2 * .05;
+		dime2 = dime2 * .10;
+		quarter2 = quarter2 * .25;
+		money = money + nickel2 + dime2 + quarter2;
 		// Tell the user how much money they have
-
+		JOptionPane.showMessageDialog(null, "You have $" + money + " dollars");
 	}
 }
-
